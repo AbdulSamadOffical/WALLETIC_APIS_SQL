fd6e7594b7f646bca2f90e0eaaedf221ab415a0e
Abdul Samad <abdulsamad@gmail.com>
2022-06-08 19:48:37 +0500

    setup db on freemysqlhosting.net

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+require('dotenv').config()
 const bodyParser = require("body-parser");
 const accountRoutes = require("./Routes/Account");
 const authRoutes = require("./Routes/Auth");
