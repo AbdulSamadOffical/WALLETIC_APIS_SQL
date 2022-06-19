5eb5ae0b1bfbcb1ec3e8005873a7fb976c73dca0
anaaf <anaaf5555@gmail.com>
2022-06-20 00:02:48 +0500

    verify user api

--- DB/connection.js
+++ DB/connection.js
@@ -12,12 +12,20 @@ const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.NAME,
   database: "walletic",
-  // process.env.NAME,
   password: process.env.PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
+// const pool = mysql.createPool({
+//   host: process.env.HOST,
+//   user: process.env.NAME,
+//   database: "walletic",
+//   password: process.env.PASSWORD,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 module.exports = pool.promise();
